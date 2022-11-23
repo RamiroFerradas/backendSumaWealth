5397fe1de9ce958e7bd53fb93a264cd954d4b16d
Ramiro <ramiferra97@gmail.com>
2022-11-23 13:19:36 -0300

    create routes airports by name & by id

--- src/Controllers/Airports/index.js
+++ src/Controllers/Airports/index.js
@@ -1,20 +1,8 @@
 const { Airport, Airline } = require("../../db");
-
+const { Op } = require("sequelize");
 const { airports } = require("../../Json/airports.json");
 
 const ERROR = "Error @ Controllers/Airports";
-
-// GET (ALL) AIRPORTS
-const getAirports = async () => {
-  //
-  try {
-    const airport = await Airport.findAll();
-    return airport;
-    //
-  } catch (e) {
-    console.error(`${ERROR}getAirports --→ ${e}`);
-  }
-};
 
 // SAVE DB (ALL) AIRPORTS
 const jsonAirports = async () => {
@@ -43,7 +31,50 @@ const jsonAirports = async () => {
   }
 };
 
+// GET (ALL) AIRPORTS
+const getAirports = async () => {
+  //
+  try {
+    const airport = await Airport.findAll();
+    return airport;
+    //
+  } catch (e) {
+    console.error(`${ERROR}getAirports --→ ${e}`);
+  }
+};
+
+// GET (ONE) AIRPORTS BY NAME
+const getAirportsById = async (id) => {
+  //
+
+  try {
+    const airportId = await Airport.findOne({
+      where: { id },
+    });
+    return airportId;
+    //
+  } catch (e) {
+    console.error(`${ERROR}getAirportByName --→ ${e}`);
+  }
+};
+
+const getAirportsByName = async (name) => {
+  console.log(name);
+
+  try {
+    const airportName = await Airport.findOne({
+      where: { airport: { [Op.iLike]: `%${name}%` } },
+    });
+    return airportName;
+    //
+  } catch (e) {
+    console.error(`${ERROR}getAirportByName --→ ${e}`);
+  }
+};
+
 module.exports = {
   getAirports,
   jsonAirports,
+  getAirportsByName,
+  getAirportsById,
 };
--- src/routes/Airports/index.js
+++ src/routes/Airports/index.js
@@ -1,23 +1,42 @@
 const { Router } = require("express");
 
-const { getAirports } = require("../../Controllers/Airports");
+const {
+  getAirports,
+  getAirportsByName,
+  getAirportsById,
+} = require("../../Controllers/Airports");
 
 const router = Router();
 
-const ERROR = `Error @ routes/Contratos --> `;
+const ERROR = `Error @ routes/Airports --> `;
 
 // -------------------------------------------
 
-// ---------- GET AIRPORTS ----------
+// ---------- GET AIRPORTS // GET AIRPORTS BY NAME ----------
 router.get("/", async (req, res) => {
   try {
-    //
-    const airports = await getAirports();
-    if (airports.length) res.json(airports);
-    else return "No se han encontrado aeropuertos.";
-    //
+    const { name } = req.query;
+    if (name) {
+      res.json(await getAirportsByName(name));
+    } else {
+      const airports = await getAirports();
+      if (airports.length) res.json(airports);
+      else return "No se han encontrado aeropuertos.";
+    }
   } catch (e) {
     res.status(400).send(ERROR, e);
+  }
+});
+
+// ---------- GET AIRPORTS BY ID ----------
+
+router.get("/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    console.log(id);
+    res.json(await getAirportsById(id));
+  } catch (e) {
+    res.status(404).send(`Error --→ ${e}`);
   }
 });
 
